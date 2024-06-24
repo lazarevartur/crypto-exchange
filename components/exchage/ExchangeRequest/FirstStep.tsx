8600fa35fdbd2856dd41da471ef2593dbbcc2bc9
lazarevartur <gracian950@gmail.com>
2024-06-24 17:26:28 +0300

    feat : swap tokens flow

--- components/exchage/ExchangeRequest/FirstStep.tsx
+++ components/exchage/ExchangeRequest/FirstStep.tsx
@@ -3,9 +3,11 @@ import { CurrencySelect } from "@/components/exchage/CurrencySelect";
 import { InputStyles } from "@/components/exchage/styles";
 import { SelectNetwork } from "@/components/exchage/SelectNetwork";
 import { useTokens } from "@/http/query/tokens";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useActiveChangeCurrency } from "@/state/activeChangeCurrency";
 import { Controller, useForm } from "react-hook-form";
+import { IReserveItem } from "@/lib/types/types";
+import { calculateExchangeAmount } from "@/app/api/_utils/utils";
 
 type Inputs = {
   from: string;
@@ -16,26 +18,79 @@ type Inputs = {
 export const FirstStep = () => {
   const { prepareTokens } = useTokens();
   const tokens = useMemo(() => prepareTokens(), [prepareTokens]);
-  const { from, to, setAmount, setNetwork } = useActiveChangeCurrency();
+  const { from, to, setAmount, setNetwork, setActiveCurrency } =
+    useActiveChangeCurrency();
 
-  const { register, watch, control } = useForm<Inputs>();
+  const [direction, setDirection] = useState<"from" | "to">("from");
+  const { register, watch, control, setValue } = useForm<Inputs>({
+    defaultValues: {
+      from: "",
+      to: "",
+    },
+  });
+  console.log(from);
+  const onChangeHandler =
+    (direction: "from" | "to") => (item: IReserveItem) => {
+      setActiveCurrency({ direction, ...item });
+    };
 
   const fromAmount = watch("from");
   const toAmount = watch("to");
   const network = watch("network");
 
   useEffect(() => {
-    setAmount({ direction: "from", amount: Number(fromAmount) });
-    setAmount({ direction: "to", amount: Number(toAmount) });
-    setNetwork(network)
-  }, [fromAmount, toAmount, network]);
+    const direction = "from";
+    setAmount({ direction, amount: Number(fromAmount) });
+  }, [fromAmount]);
+
+  useEffect(() => {
+    const direction = "to";
+    setAmount({ direction, amount: Number(toAmount) });
+  }, [toAmount]);
+
+  useEffect(() => {
+    if (direction === "from") {
+      if (from?.price && to?.price) {
+        const totalPrice = calculateExchangeAmount({
+          amount: Number(fromAmount),
+          sourceToUsdRate: from.price,
+          targetToUsdRate: to.price,
+        });
+        setValue("to", totalPrice.toString());
+      }
+    }
+    if (direction === "to") {
+      if (from?.price && to?.price) {
+        const totalPrice = calculateExchangeAmount({
+          amount: Number(toAmount),
+          sourceToUsdRate: to.price,
+          targetToUsdRate: from.price,
+        });
+        setValue("from", totalPrice.toString());
+      }
+    }
+  }, [direction, from, to, fromAmount, toAmount]);
+
+  useEffect(() => {
+    setNetwork(network);
+  }, [network]);
 
   return (
     <>
       <Flex flexDir="column">
         <Flex flexDir="column" gap="10px">
-          <CurrencySelect options={tokens} defaultValue={from} />
-          <Input {...register("from")} type="number" sx={InputStyles} />
+          <CurrencySelect
+            options={tokens}
+            defaultValue={from}
+            onChange={onChangeHandler("from")}
+          />
+          <Input
+            {...register("from")}
+            type="number"
+            sx={InputStyles}
+            onFocus={() => setDirection("from")}
+            isDisabled={!from}
+          />
         </Flex>
         <Flex
           flexDir="column"
@@ -45,11 +100,25 @@ export const FirstStep = () => {
           color="#737373"
         >
           <Text>Скидка: 0 % (0.00 RUB)</Text>
-          <Text>Минимум: 30000 Максимум: 900000</Text>
+          {from?.min && from?.amount && (
+            <Text>
+              Минимум: {from?.min} Максимум: {from?.amount}
+            </Text>
+          )}
         </Flex>
         <Flex flexDir="column" gap="10px">
-          <CurrencySelect options={tokens} defaultValue={to} />
-          <Input {...register("to")} type="number" sx={InputStyles} />
+          <CurrencySelect
+            options={tokens}
+            defaultValue={to}
+            onChange={onChangeHandler("to")}
+          />
+          <Input
+            {...register("to")}
+            type="number"
+            sx={InputStyles}
+            onFocus={() => setDirection("to")}
+            isDisabled={!to}
+          />
         </Flex>
       </Flex>
       <Flex w="100%" mt="20px" justifyContent="center">
