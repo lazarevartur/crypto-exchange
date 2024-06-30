60e3ec7372e98cbb9529ed9212bbdbe164b00696
samfisher7777 <artur.z@blaize.tech>
2024-06-30 19:26:20 +0300

    feat: RulesPage, contact page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,7 +1,48 @@
+import { Container, Flex, Heading, Text } from "@chakra-ui/react";
+
 export default function ContactPage() {
   return (
-      <>
-        <h1>ContactPage</h1>
-      </>
+    <Container
+      as="section"
+      maxW="container.xl"
+      py="44px"
+      display="flex"
+      flexDir="column"
+      gap="24px"
+    >
+      <Heading
+        textAlign="center"
+        as="h2"
+        fontSize="26px"
+        fontWeight={600}
+        lineHeight="120%"
+        color="#212121"
+        mb="10px"
+      >
+        Контакты
+      </Heading>
+      <Flex gap="12px" flexDirection="column">
+        <Text fontWeight={600}>Для рекламы и сотрудничества:</Text>
+        <Text color="#fcbf11" fontWeight={600}>
+          partners@cosmochanger.cc
+        </Text>
+        <Text fontWeight={600}>Поддержка по почте:</Text>
+        <Text color="#fcbf11" fontWeight={600}>
+          support@cosmochanger.cc
+        </Text>
+        <Text fontWeight={600}>Резервная почта:</Text>
+        <Text color="#fcbf11" fontWeight={600}>
+          reservecosmochanger@protonmail.com
+        </Text>
+        <Text fontWeight={600}>Для рекламы и сотрудничества:</Text>
+        <Text color="#fcbf11" fontWeight={600}>
+          partners@cosmochanger.cc
+        </Text>
+        <Text fontWeight={600}>Для рекламы и сотрудничества:</Text>
+        <Text color="#fcbf11" fontWeight={600}>
+          partners@cosmochanger.cc
+        </Text>
+      </Flex>
+    </Container>
   );
 }
--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,7 +1,17 @@
+import { Container } from "@chakra-ui/react";
+
 export default function FAQPage() {
   return (
-    <>
-      <h1>FAQ</h1>
-    </>
+    // eslint-disable-next-line react/jsx-no-undef
+    <Container
+      as="section"
+      maxW="container.xl"
+      py="44px"
+      display="flex"
+      flexDir="column"
+      gap="24px"
+    >
+      FAQPage
+    </Container>
   );
 }
